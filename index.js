21abe7fe482cb7515853d9f9c8d18146de4c14f8
u9g <43508353+u9g@users.noreply.github.com>
2021-06-07 10:44:24 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs/promises')
+
+main()
+
+async function main () {
+  const [skipped, , ...rest] = (await fs.readFile('./data.txt', 'utf-8')).split('\r\n')
+  const obj = {}
+  const parsedSkipped = compileSkippedObj(skipped)
+  obj[parsedSkipped[0]] = parsedSkipped[1]
+  rest
+    .map(o => o.match(/private static final Map<String, String> (.+) = ImmutableMap\.builder\(\)(.+)\.build()/))
+    .map(o => {
+      let data = o[2].split('.put')
+      data.shift()
+      data = data.map(o => {
+        const [, oldName, newName] = o.match(/\("(.+)", "(.+)"\)/)
+        return [oldName, newName]
+      })
+      return [o[1], data]
+    })
+    .forEach(([name, data]) => {
+      obj[name] = data
+    })
+  await fs.writeFile('output.json', JSON.stringify(obj, null, 2))
+}
+
+function compileSkippedObj (skipped) {
+  let [, name, data] = skipped.match(/private static final Set<String> (.+) = ImmutableSet\.builder\(\)(.+)\.build()/)
+  data = data.split('.add')
+  data.shift()
+  data = data.map(o => o.match(/\("(.+)"\)/)[1])
+  return [name, data]
+}
